54c46100eaf4077c2363febf65b70d98d5ab64f2
twik <twik@nomail.org>
2025-01-01 18:54:48 +0100

    First draft of the weblayout

--- monithor/views.py
+++ monithor/views.py
@@ -7,4 +7,4 @@ def settings_view(request):
 def index_view(request):
     items1 = ["Item 1A", "Item 1B", "Item 1C"]
     items2 = ["Item 2A", "Item 2B", "Item 2C"]
-    return render(request, 'lists.html', {'list1': items1, 'list2': items2})
+    return render(request, 'base.html', {'list1': items1, 'list2': items2})
